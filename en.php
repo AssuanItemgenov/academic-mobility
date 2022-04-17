9c20a48e29184bd8ebf8c67c27ed79d4d36f008d
AssuanItemgenov <87804037+AssuanItemgenov@users.noreply.github.com>
2022-04-17 22:47:32 +0600

    Add files via upload

---
+++ en.php
@@ -0,0 +1,219 @@
+<?php
+$welPWords = [
+    'title' => 'Academic mobility',
+    'lang' => 'EN',
+    'login' => 'Login'
+];
+
+$authAndRegPWords = [
+    'title' => 'Authorization and registration', 
+    'login' => 'Login', 
+    'password' => 'Password', 
+    'PassErrQ' => 'Forgot your password?', 
+    'question1' => 'Not registered yet?', 
+    'registration' => 'Registration', 
+    'name' => 'Name', 
+    'surname' => 'Surname', 
+    'userType' => 'Type of user', 
+    'userType1' => 'Teacher', 
+    'userType2' => 'Student', 
+    'passwordConfirm' => 'Confirm the password', 
+    'question2' => 'Already Registered?', 
+    'PassErr' => 'Enter the correct password', 
+    'LogAndPassErr' => 'Enter login and password', 
+    'emailErr' => 'User with this email is already registered', 
+    'PassSuccErr' => 'Your password has been successfully updated', 
+    'RegSuccErr' => 'You have successfully registered', 
+    'downloadAuthErr' => 'Please, log in',
+    'mismatchErr' => 'Password mismatch', 
+    'notRegErr' => 'Please, register', 
+    'fieldErr' => 'Fill in all the fields', 
+    'emailConfirmErr' => 'An email confirmation error has occurred, please register again'
+];
+
+$restorePassPWords = [
+    'title' => 'Password reset', 
+    'information' => 'To reset your password, enter your email address and new password', 
+    'newPassTitle' => 'Enter a new password', 
+    'newConfPassTitle' => 'Confirm the password', 
+    'password' => 'Password', 
+    'button' => 'Reset', 
+    'notFoundEmailErr' => 'User with this email is not registered', 
+    'mismatchErr' => 'Password mismatch', 
+    'LogAndPassErr' => 'Enter login and password', 
+    'restoreErr' => 'An email confirmation error has occurred, please go through the password recovery process again'
+];
+
+$deskPWords = [
+    'title' => 'Announcement', 
+    'emailSend' => 'To confirm your email, an email was sent to your email, follow the link in the email to complete the registration', 
+    'registrationNotEnd' => 'You have not completed the registration process. A letter was sent to your email, follow the link in the letter to complete the registration', 
+    'emailSendForRestore' => 'To confirm your email, an email has been sent to your email address. Follow the link in the email to complete the password reset process'
+];
+
+$activationPWords = [
+    'title' => 'Activation', 
+    'passMessage' => 'Password changed successfully', 
+    'regMessage' => 'You have successfully registered', 
+    'infoString' => 'Click "Next" to authorize', 
+    'button' => 'Next'
+];
+
+$welcomePWords = [
+    'title' => 'Academic mobility', 
+    'admissionErr' => "You don't have access", 
+    'linkToFillIn' => 'Apply', 
+    'linkToResult' => 'My details', 
+    'linkToRequests' => 'Applications', 
+    'button' => 'Logout'
+];
+
+$emailRegWords = [
+    'letterTitle' => 'Registration on the website Academic mobility',
+    'letterHello' => 'Hello!',  
+    'linkInLetter' => 'Click to complete registration', 
+    'nameOFPost' => 'Academic mobility KazNU', 
+];
+
+$emailRePassWords = [
+    'letterTitle' => 'Password recovery',  
+    'linkInLetter' => 'Click to complete the password reset', 
+    'nameOFPost' => 'Academic mobility KazNU', 
+    'letterText' => "This letter was sent to you to reset your password on the Academic mobility website. If it's not you, please ignore the email."
+];
+
+$applicationPWords = [
+    'title' => 'Application submission', 
+    'btnLogout' => 'Logout', 
+    'information' => 'Enter your personal data', 
+    'card1' => 'Personal information', 
+    'card2' => 'Information on sending university', 
+    'card3' => 'Mobility', 
+    'card4' => 'Information on education', 
+    'nameLabel' => 'Name', 
+    'surnameLabel' => 'Surname',
+    'sexLabel' => 'Sex', 
+    'sexVar1' => 'Male', 
+    'sexVar2' => 'Female',
+    'birthDateLabel' => 'Date of birth', 
+    'placeOfBithrLabel' => 'Place of birth', 
+    'citizenshipLabel' => 'Citizenship', 
+    'nationalityLabel' => 'Nationality', 
+    'passportNumberLabel' => 'Passport number', 
+    'issueDateLabel' => 'Issue date of passport', 
+    'expiryDateLabel' => 'Expiry  date of passport', 
+    'addressOfResidenceLabel' => 'Address of residence', 
+    'copyOfPassportLabel' => 'Attach a copy of your passport (PDF)',
+    'btnForFile' => 'Select a file', 
+    'infForFile' => 'No file selected', 
+    'universityLabel1' => 'Position and affiliation', //для преподавателей
+    'universityValue' => 'Enter the name of the university',
+    'universityLabel2' => 'Sending University', //для студентов
+    'facultyFrom' => 'Faculty of Education', 
+    'facultyFromValue' => 'Enter the name of the faculty', 
+    'degreeLabel' => 'Degree', 
+    'majorLabel' => 'Major', 
+    'majorValue' => 'Enter the name of the major', 
+    'nominationLetterLabel' => 'Attach the nomination letter of your University (PDF)', 
+    'transcriptLabel' => 'Attach your transcript of records (PDF)', 
+    'certificateOfEmploymentLabel' => 'Attach a certificate of employment (PDF)', 
+    'academicDegreeLabel' => 'Academic degree',
+    'academDegVar1' => 'Bachelor', 
+    'academDegVar2' => 'Master', 
+    'academDegVar3' => 'PHD', 
+    'educationDocumentLabel' => 'Attach education document (PDF)', 
+    'CVLabel' => 'Attach your CV (PDF)', 
+    'publicationsLabel' => 'Attach a list of your publications (PDF)', 
+    'purposeLabel' => 'Purpose of visit', 
+    'purposeVar1' => 'Lecturing', 
+    'purposeVar2' => 'Research work', 
+    'purposeVar3' => 'Other: specify your option', 
+    'purposeVar4' => 'Internship', 
+    'purposeVar5' => 'Semester training', 
+    'purposeVar6Value' => 'For example: summer school, language courses', 
+    'mobilityProgramLabel' => 'Under what program will your mobility be implemented?', 
+    'mobilityProgramVar1' => 'Erasmus+*', 
+    'mobilityProgramVar2' => 'Mevlana*', 
+    'mobilityProgramVar3' => 'Orhun*', 
+    'mobilityProgramVar4' => 'Inter-university exchange', 
+    'mobilityProgramVar5' => 'USSC*', 
+    'mobilityProgramVar6' => 'SUSNG*', 
+    'agreementLabel' => 'Attach learning agreement  (only for semester studies)', 
+    'facultyLabel' => 'Indicate the faculty on which the mobility will be carried out', 
+    'mobilityLabel' => 'Form of Mobility', 
+    'mobilityVar1' => 'Online', 
+    'mobilityVar2' => 'Offline', 
+    'periodLabel' => 'Period of stay', 
+    'periodVar1' => 'from',
+    'periodVar2' => 'to',
+    'visaSupportLabel' => 'Do you need visa support? ', 
+    'supportVar1' => 'Yes', 
+    'supportVar2' => 'No', 
+    'supportValue' => 'Place of visa issue (country and city)', 
+    'dormitoryLabel' => 'Do you need a place at the dormitory?', 
+    'fillAllErr' => 'Fill in all fields and attach all files', 
+    'fillErr' => 'Enter your personal data', 
+    'notHaveDataErr' => 'You have no data entered', 
+    'button' => 'Ready', 
+    'btnNext' => 'Next', 
+    'btnBack' => 'Back'
+];
+
+$dataPWords = [
+    'title' => 'Your data', 
+    'btnLogout' => 'Logout', 
+    'informationForData' => 'Your personal data', 
+    'facultyLabel' => 'Faculty', 
+    'visaSupportLabel' => 'Visa support', 
+    'buttonForData' => 'Change', 
+    'informationForFile' => 'Your attachments', 
+    'passportLabel' => 'Copy of the passport',
+    'buttonForChange' => 'Change', 
+    'certificateOfEmploymentLabel' => 'Certificate of employment', 
+    'educationDocumentLabel' => 'Education document  ', 
+    'nominationLetterLabel' => 'Nomination letter', 
+    'transcriptLabel' => 'Transcript', 
+    'agreementLabel' => 'Learning agreement', 
+    'mobilityProgramLabel' => 'Mobility program', 
+    'CVLabel' => 'CV', 
+    'publicationsLabel' => 'Publications ', 
+    'dormitoryLabel' => 'The need for a dormitory', 
+    'buttonForFile' => 'Download files', 
+    'haveDataErr' => 'You already have data', 
+    'fillAllErr' => 'Please, fill in all fields', 
+];
+
+$requestsPWOrds = [
+    'title' => 'Applications', 
+    'regDateLabel' => 'Registration date', 
+    'facultyLabel' => 'Faculty, where mobility will take place', 
+    'button' => 'More details', 
+    'dateOfData' => 'Application date', 
+    'btnClear' => 'Clear filter'
+];
+
+$moreDataPWOrds = [
+    'title' => 'Full details of the applicant', 
+    'infor' => 'Personal Information', 
+    'btn' => 'Download', 
+    'btnDownloadAll' => 'Download all files', 
+    'visaSupportLabel' => 'Need for visa support', 
+    'fileNotFound' => 'File not found'
+];
+
+$preSendPWords = [
+    'title' => 'Your data', 
+    'information' => 'Your personal data', 
+    'warning' => '«Before submitting the data, please check the correctness of the information you have provided. 
+    Once you click on «Send» you will not be able to edit your details. 
+    If you need to change the data click on «Edit». If in doubt, you can come back to them later, your data is saved»', 
+    'buttonChange' => 'Edit', 
+    'buttonSend' => 'Send', 
+    'haveDataErr' => 'You have already entered your data', 
+    'upDataErr' => 'Data updated', 
+    'upFilesErr' => 'Files updated', 
+    'notFoud' => 'No files found', 
+    'dataSent' => 'Application sent', 
+    'modalWarning' => 'I confirm the accuracy and correctness of the information I have filled in.', 
+    'buttonConfirm' => 'Confirm'
+];
---
+++ kz.php
@@ -0,0 +1,219 @@
+<?php
+$welPWords = [
+    'title' => 'Академиялық ұтқырлық',
+    'lang' => 'KZ',
+    'login' => 'Кіру'
+];
+
+$authAndRegPWords = [
+    'title' => 'Кіру және тіркелу', 
+    'login' => 'Кіру', 
+    'password' => 'Құпия сөз',
+    'PassErrQ' => 'Құпия сөзді ұмыттыңыз ба?', 
+    'question1' => 'Тіркелмегенсіз ба?', 
+    'registration' => 'Тіркелу', 
+    'name' => 'Аты', 
+    'surname' => 'Тегі', 
+    'userType' => 'Өтініш беруші', 
+    'userType1' => 'Оқытушы', 
+    'userType2' => 'Студент', 
+    'passwordConfirm' => 'Құпия сөзді қайта теріңіз', 
+    'question2' => 'Тіркелгенсіз ба?', 
+    'PassErr' => 'Құпия сөзді қате тердіңіз', 
+    'LogAndPassErr' => 'Логин мен құпия сөзді теріңіз', 
+    'emailErr' => 'Бұндай email-мен қолданушы тіркелінген', 
+    'PassSuccErr' => 'Құпия сөз жаңарды', 
+    'RegSuccErr' => 'Тіркелу сәтті аяқталды', 
+    'downloadAuthErr' => 'Жүйеге кіру қажет',
+    'mismatchErr' => 'Құпия сөдер сәйкес келмейді', 
+    'notRegErr' => 'Тіркеліңіз', 
+    'fieldErr' => 'Барлық жолдарды толтырыңыз', 
+    'emailConfirmErr' => 'Электрондық поштаны растау қатесі орын алды, қайта тіркеліңіз'
+];
+
+$restorePassPWords = [
+    'title' => 'Құпия сөзді жаңарту', 
+    'information' => 'Құпия сөзді жаңарту үшін, email мен жаңа құпия сөзді теріңіз', 
+    'newPassTitle' => 'Құпия сөзді теріңіз', 
+    'newConfPassTitle' => 'Құпия сөзді қайта теріңіз', 
+    'password' => 'Құпия сөз', 
+    'button' => 'Құпия сөзді жаңарту', 
+    'notFoundEmailErr' => 'Бұндай email жүйеде тіркелмеген', 
+    'mismatchErr' => 'Құпия сөздер сәйкес келмейді', 
+    'LogAndPassErr' => 'Логин мен құпия сөзді теріңіз', 
+    'restoreErr' => 'Электрондық поштаны растау қатесі орын алды, құпия сөзді жаңарту процесін қайтадан өтіңіз'
+];
+
+$deskPWords = [
+    'title' => 'Ақпарат', 
+    'emailSend' => 'Сіздің электрондық поштаңызға хат жіберілді. Тіркеуді аяқтау үшін, сілтеме арқылы өтіңіз', 
+    'registrationNotEnd' => 'Сіз тіркеу процесін аяқтаған жоқсыз. Сіздің электрондық поштаңызға хат жіберілді, тіркеуді аяқтау үшін, хаттағы сілтеме арқылы өтіңіз', 
+    'emailSendForRestore' => 'Сіздің электрондық поштаңызға хат жіберілді. Құпия сөзді жаңарту процесін аяқтау үшін, электрондық поштадағы сілтеме арқылы өтіңіз'
+];
+
+$activationPWords = [
+    'title' => 'Белсендіру', 
+    'passMessage' => 'Құпия сөз сәтті өзгертілді', 
+    'regMessage' => 'Сіз сәтті тіркелдіңіз', 
+    'infoString' => 'Кіру үшін "Жалғастыру" пернесін бысыңыз', 
+    'button' => 'Жалғастыру'
+];
+
+$welcomePWords = [
+    'title' => 'Академиялық ұтқырлық', 
+    'admissionErr' => 'Cізде рұқсат жоқ', 
+    'linkToFillIn' => 'Тапсырыс беру', 
+    'linkToResult' => 'Менің мәліметтерім', 
+    'linkToRequests' => 'Тапсырыстар', 
+    'button' => 'Шығу'
+];
+
+$emailRegWords = [
+    'letterTitle' => 'Академиялық ұтқырлық сайтында тіркеу',
+    'letterHello' => 'Сәлеметсізбе!',  
+    'linkInLetter' => 'Тіркеуді аяқтау үшін басыңыз', 
+    'nameOFPost' => 'Академиялық ұтқырлық ҚазҰУ', 
+];
+
+$emailRePassWords = [
+    'letterTitle' => 'Құпия сөзді жаңарту',  
+    'linkInLetter' => 'Құпия сөзді жаңарту үшін басыңыз', 
+    'nameOFPost' => 'Академиялық ұтқырлық ҚазҰУ', 
+    'letterText' => 'Бұл хат сізге Академиялық ұтқырлық веб-сайтында құпия сөзді жаңарту үшін жіберілді. Егер құпия сөзді жаңартушы сіз емес болсаңыз, хатты елемеңіз.'
+];
+
+$applicationPWords = [
+    'title' => 'Өтініш беру', 
+    'btnLogout' => 'Шығу', 
+    'information' => 'Жеке деректеріңізді енгізіңіз', 
+    'card1' => 'Жеке мәлімет', 
+    'card2' => 'Жіберіп жатқан университет тупалы мәлімет', 
+    'card3' => 'Ұтқырлық', 
+    'card4' => 'Білім туралы мәлімет', 
+    'nameLabel' => 'Аты', 
+    'surnameLabel' => 'Тегі', 
+    'sexLabel' => 'Жынысы', 
+    'sexVar1' => 'Ер', 
+    'sexVar2' => 'Әйел',
+    'birthDateLabel' => 'Туған күні', 
+    'placeOfBithrLabel' => 'Туған жері', 
+    'citizenshipLabel' => 'Азаматтығы', 
+    'nationalityLabel' => 'Ұлты', 
+    'passportNumberLabel' => 'Паспорт номері', 
+    'issueDateLabel' => 'Құжаттың берілген күні', 
+    'expiryDateLabel' => 'Аяқталу мерзімі', 
+    'addressOfResidenceLabel' => 'Тұрғылықты жері', 
+    'copyOfPassportLabel' => 'Паспортыңыздың көшірмесін жүктеңіз (PDF)',
+    'btnForFile' => 'Файлды таңдаңыз', 
+    'infForFile' => 'Таңдалған файл жоқ', 
+    'universityLabel1' => 'Жұмыс орны', 
+    'universityValue' => 'Университет атауын енгізіңіз', 
+    'universityLabel2' => 'Оқу орны', 
+    'facultyFrom' => 'Факультет', 
+    'facultyFromValue' => 'Факультет атауын енгізіңіз', 
+    'degreeLabel' => 'Деңгейі', 
+    'majorLabel' => 'Мамандық', 
+    'majorValue' => 'Мамандық атауын енгізіңіз', 
+    'nominationLetterLabel' => 'Оқу орныңыздың номинация туралы хатын жүктеңіз (PDF)', 
+    'transcriptLabel' => 'Үлгеріміңіз туралы транскриптті жүктеңіз (PDF)', 
+    'certificateOfEmploymentLabel' => 'Жұмыс орнынан анықтама жүктеңіз (PDF)', 
+    'academicDegreeLabel' => 'Ғылыми дәрежесі',
+    'academDegVar1' => 'Бакалавр', 
+    'academDegVar2' => 'Магистр', 
+    'academDegVar3' => 'PHD', 
+    'educationDocumentLabel' => 'Білім туралы құжатыңызды жүктеңіз (PDF)', 
+    'CVLabel' => 'Резюмеңізді жүктеңіз (PDF)', 
+    'publicationsLabel' => 'Мақалалар тізімін жүктеңіз (PDF)', 
+    'purposeLabel' => 'Келу мақсаты', 
+    'purposeVar1' => 'Дәріс оқу', 
+    'purposeVar2' => 'Зерттеу жұмысы', 
+    'purposeVar3' => 'Басқа: өз опцияңызды көрсетіңіз', 
+    'purposeVar4' => 'Тәжірибе жинау', 
+    'purposeVar5' => 'Семестрлік оқу', 
+    'purposeVar6Value' => 'Мысалы: жазғы мектеп, тілдік курстар', 
+    'facultyLabel' => 'Ұтқырлық іске асырылатын факультетті көрсетіңіз', 
+    'mobilityProgramLabel' => 'Сіздің мобильділік қандай бағдарлама бойынша жүзеге асады?', 
+    'mobilityProgramVar1' => 'Эразмус+*', 
+    'mobilityProgramVar2' => 'Мевлана*', 
+    'mobilityProgramVar3' => 'Орхун*', 
+    'mobilityProgramVar4' => 'Университетаралық алмасу', 
+    'mobilityProgramVar5' => 'УШОС*', 
+    'mobilityProgramVar6' => 'СУСНГ*', 
+    'agreementLabel' => 'Оқуға келісім шартты жүктеңіз (тек семестрлік оқу үшін)', 
+    'mobilityLabel' => 'Ұтқырлық формасы', 
+    'mobilityVar1' => 'Онлайн', 
+    'mobilityVar2' => 'Офлайн', 
+    'periodLabel' => 'Ұтқырлық Мерзімі', 
+    'periodVar1' => 'Басталуы',
+    'periodVar2' => 'Аяқталуы',
+    'visaSupportLabel' => 'Сізге визалық қолдау қажет пе?', 
+    'supportVar1' => 'Иә', 
+    'supportVar2' => 'Жоқ', 
+    'supportValue' => 'Виза алу жері (мемлекет пен қала)', 
+    'dormitoryLabel' => 'Сізге жатақханадан орын қажет пе?', 
+    'fillAllErr' => 'Барлық өрістерді толтырыңыз және барлық файлдарды жүктеңіз', 
+    'fillErr' => 'Жеке деректеріңізді енгізіңіз', 
+    'notHaveDataErr' => 'Сізде енгізілген деректер жоқ', 
+    'button' => 'Дайын', 
+    'btnNext' => 'Ары қарай', 
+    'btnBack' => 'Артқа'
+];
+
+$dataPWords = [
+    'title' => 'Сіздің деректеріңіз', 
+    'btnLogout' => 'Шығу', 
+    'informationForData' => 'Сіздің жеке деректеріңіз', 
+    'facultyLabel' => 'Факультет', 
+    'visaSupportLabel' => 'Визалық қолдау', 
+    'buttonForData' => 'Өзгерістер енгізу', 
+    'informationForFile' => 'Сіз жүктеген құжаттар', 
+    'passportLabel' => 'Паспорт көшірмесі',
+    'buttonForChange' => 'Өзгерту', 
+    'certificateOfEmploymentLabel' => 'Жұмыс орнынан анықтама', 
+    'educationDocumentLabel' => 'Білім туралы құжат', 
+    'nominationLetterLabel' => 'Номинация туралы хат', 
+    'transcriptLabel' => 'Транскрипт', 
+    'mobilityProgramLabel' => 'Мобильділік бағдарламасы', 
+    'agreementLabel' => 'Оқуға келісім шарт', 
+    'CVLabel' => 'Резюме', 
+    'publicationsLabel' => 'Мақалалар тізімін ', 
+    'dormitoryLabel' => 'Жатақхана қажеттілігі', 
+    'buttonForFile' => 'Файлдарды жүктеу', 
+    'haveDataErr' => 'Сізде деректер бар', 
+    'fillAllErr' => 'Барлық өрістерді толтырыңыз', 
+];
+
+$requestsPWOrds = [
+    'title' => 'Өтініш', 
+    'regDateLabel' => 'Тіркеу күні', 
+    'facultyLabel' => 'Ұтқырлық өтетін факультет', 
+    'button' => 'Толығырақ', 
+    'dateOfData' => 'Өтініш берілген күн', 
+    'btnClear' => 'Сүзгіні тазалау'
+];
+
+$moreDataPWOrds = [
+    'title' => 'Өтініш беруші туралы толық мәліметтер', 
+    'infor' => 'Жеке мәліметтер', 
+    'btn' => 'Жүктеу', 
+    'btnDownloadAll' => 'Барлық файлдарды жүктеу', 
+    'visaSupportLabel' => 'Визалық қолдау қажеттілігі', 
+    'fileNotFound' => 'Файл табылмады'
+];
+
+$preSendPWords = [
+    'title' => 'Сіздің деректеріңіз', 
+    'information' => 'Сіздің жеке деректеріңіз', 
+    'warning' => '«Деректерді жібермес бұрын, сіз берген ақпараттың дұрыстығын тексеріңіз.
+    «Жіберу» түймесін басқаннан кейін сіз мәліметтерді өңдей алмайсыз.
+    Егер сізге деректерді өзгерту қажет болса, «Өңдеу» түймесін басыңыз. Егер күмәндансаңыз, оларға кейінірек оралуға болады, сіздің деректеріңіз сақталынып тұр».', 
+    'buttonChange' => 'Өңдеу', 
+    'buttonSend' => 'Жіберу', 
+    'haveDataErr' => 'Сіз деректеріңізді енгіздіңіз', 
+    'upDataErr' => 'Деректер жаңартылды', 
+    'upFilesErr' => 'Файлдар жаңартылды', 
+    'notFoud' => 'Файлдар табылмады', 
+    'dataSent' => 'Өтініш жіберілді', 
+    'modalWarning' => 'Мен толтырған ақпараттың дұрыстығын растаймын.', 
+    'buttonConfirm' => 'Растау'
+];
